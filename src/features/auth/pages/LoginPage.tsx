230f2e9c0c0856806a683b15aab49a2ee440db96
aswin <nuzhumaki9@gmail.com>
2026-03-09 21:39:33 +0530

    feat(login): redirect the user to the home page after the succesfull login

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import { Login } from "../components/Login";
 import { api } from "@/lib/axios";
+import { useNavigate } from "react-router";
 
 export function LoginPage() {
   const [isLoading, setIsLoading] = useState(false);
-
+  const navigate = useNavigate();
   const onSubmit = async (data: any): Promise<void> => {
     setIsLoading(true);
     try {
       const res = await api.post(`/login`, data);
       console.log(res);
+
+      // TODO : save the accesstoken to the redux
+
+      // redirect the user into home page
+      navigate("/");
     } catch (error) {
       throw error;
     } finally {
