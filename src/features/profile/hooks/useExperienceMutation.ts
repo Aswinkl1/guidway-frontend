f266c8a06e648e53a5c491f8d7cd4c285eff3f2b
aswin <nuzhumaki9@gmail.com>
2026-05-09 18:34:01 +0530

    feat: add useEditExperience hook for managing mentor experience updates

--- src/features/profile/hooks/useExperienceMutation.ts
+++ src/features/profile/hooks/useExperienceMutation.ts
@@ -1,6 +1,9 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import type { ExperienceFormData } from "../schemas/experience.schema";
-import { AddExperience } from "../services/mentorServices";
+import type {
+  EditExperienceType,
+  ExperienceFormData,
+} from "../schemas/experience.schema";
+import { AddExperience, EditExperience } from "../services/mentorServices";
 import toast from "react-hot-toast";
 import { QUERYKEY } from "./keys";
 
@@ -12,6 +15,20 @@ export const useAddExperience = () => {
 
     onSuccess: () => {
       toast.success("Experience added!");
+      queryClient.invalidateQueries({ queryKey: QUERYKEY.all });
+    },
+    // TODO optimitic update shound be done after you finsish the profile
+  });
+};
+
+export const useEditExperience = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (data: EditExperienceType) => EditExperience(data),
+
+    onSuccess: () => {
+      toast.success("Experience edited!");
       queryClient.invalidateQueries({ queryKey: QUERYKEY.all });
     },
     // TODO optimitic update shound be done after you finsish the profile
