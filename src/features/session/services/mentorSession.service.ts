70350944fa49b4bc9269aba2ae5f9a535a742100
aswin <nuzhumaki9@gmail.com>
2026-05-14 21:34:58 +0530

    Add session management functions and filterProps type for session queries

--- src/features/session/services/mentorSession.service.ts
+++ src/features/session/services/mentorSession.service.ts
@@ -1,8 +1,33 @@
 import { ROUTES } from "@/constants/apiRoutes";
 import { api } from "@/lib/axios";
-import type { CreateSessionDTO } from "../schema/session.dto";
+import type {
+  CreateSessionDTO,
+  DeleteSessionDTO,
+  editSessionDTO,
+} from "../schema/session.dto";
+import type { filterProps } from "../types/session.types";
 
 export const addSession = async (data: CreateSessionDTO) => {
   const response = await api.post(ROUTES.MENTOR.SESSION.ROOT, data);
+  return response.data;
+};
+
+export const editSession = async (data: editSessionDTO) => {
+  const response = await api.put(
+    `${ROUTES.MENTOR.SESSION.ROOT}/${data.id}`,
+    data,
+  );
+  return response.data;
+};
+
+export const deleteSession = async (data: DeleteSessionDTO) => {
+  const response = await api.delete(`${ROUTES.MENTOR.SESSION.ROOT}/${data.id}`);
+  return response.data;
+};
+
+export const getAllSessions = async (filter: filterProps) => {
+  const response = await api.get(
+    `${ROUTES.MENTOR.SESSION.ROOT}?search=${filter.search}&page=${filter.page}&limit=${filter.limit}`,
+  );
   return response.data;
 };
