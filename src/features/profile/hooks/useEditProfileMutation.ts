2fc959eeb83573cfaa73a4e39b0809cac4dd01c9
aswin <nuzhumaki9@gmail.com>
2026-05-19 23:34:28 +0530

    Add useMentorVisibilityInProfile hook for managing mentor visibility status

--- src/features/profile/hooks/useEditProfileMutation.ts
+++ src/features/profile/hooks/useEditProfileMutation.ts
@@ -2,6 +2,8 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { EditProfile } from "../services/mentorServices";
 import { QUERYKEY } from "./keys";
 import type { EditProfileFormData } from "../schemas/edit-profile.schema";
+import type { UpdateVisibilityDTO } from "../types/settings.types";
+import { mentorStatusChanege } from "../services/settings.services";
 
 export const useEditProfile = () => {
   const queryClient = useQueryClient();
@@ -9,6 +11,16 @@ export const useEditProfile = () => {
   return useMutation({
     mutationFn: (data: EditProfileFormData) => EditProfile(data),
 
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: QUERYKEY.all });
+    },
+  });
+};
+
+export const useMentorVisibilityInProfile = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data: UpdateVisibilityDTO) => mentorStatusChanege(data),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: QUERYKEY.all });
     },
