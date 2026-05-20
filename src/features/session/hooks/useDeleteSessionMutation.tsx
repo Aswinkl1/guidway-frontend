203fc5c3c3c34bda04cc16a87f77bb8cfea709a9
aswin <nuzhumaki9@gmail.com>
2026-05-20 13:02:31 +0530

    Refactor useDeleteSessionMutation to include query invalidation on successful session deletion

--- src/features/session/hooks/useDeleteSessionMutation.tsx
+++ src/features/session/hooks/useDeleteSessionMutation.tsx
@@ -1,12 +1,19 @@
-import { QueryClient, useMutation } from "@tanstack/react-query";
+import {
+  QueryClient,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
 import { deleteSession } from "../services/mentorSession.service";
 import toast from "react-hot-toast";
+import { sessionKeys } from "./useGetSessionQuery";
 
 export const useDeleteSessionMutation = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: deleteSession,
     onSuccess: () => {
       toast.success("succesfully edited");
+      queryClient.invalidateQueries({ queryKey: sessionKeys.all });
     },
   });
 };
