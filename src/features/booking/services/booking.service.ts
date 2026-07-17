590aa3413022ac7e2939b8723c0aad0036aae86a
aswin <nuzhumaki9@gmail.com>
2026-07-17 20:53:38 +0530

    feat(review): Add deleteReview function to handle review deletion

--- src/features/booking/services/booking.service.ts
+++ src/features/booking/services/booking.service.ts
@@ -12,6 +12,7 @@ import type { HoldSlotDto } from "../dto/createOrder.dto";
 import type { MenteeBookingDetailsOutput } from "../types/bookingDetails.types";
 import type { rescheduleBookingDto } from "../dto/reschedule.dto";
 import type { addReviewDto } from "../dto/review.dto";
+import { AwardIcon } from "lucide-react";
 
 export const getSlots = async (mentorId: string, date: string) => {
   console.log("jkdfkladjlk");
@@ -137,5 +138,10 @@ export const rescheduleBooking = async (data: rescheduleBookingDto) => {
 
 export const addReview = async (data: addReviewDto) => {
   const res = await api.post("/api/v1/review", data);
+  return res.data.result;
+};
+
+export const deleteReview = async (id: string) => {
+  const res = await api.delete("/api/v1/review" + id);
   return res.data.result;
 };
